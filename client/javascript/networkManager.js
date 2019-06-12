e2fe0ee930fce345de1d35c8dde7c8fff61987fe
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-06-12 16:02:02 +0100

    Player animations & Background animations

--- client/javascript/networkManager.js
+++ client/javascript/networkManager.js
@@ -1,24 +1,34 @@
 export default class NetworkManager {    
-    
+
     // External Functions
     addPlayer(self, playerInfo) {
 
         // Use physics object to enable arcade physics with our ship.
         // Set origin of the object to be the centre rather than the top left -> This allows us to rotate around the origin with ease.
         // Set scale of object (object size)
-        self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(100, 80);
+        self.anims.create({
+            key: 'launch',
+            frames: [
+                { key: 'player_anim_1' },
+                { key: 'player_anim_2' },
+                { key: 'player_anim_3' },
+                { key: 'player_anim_4' },
+                { key: 'player_anim_5' },
+                { key: 'player_anim_6' },
+                { key: 'player_anim_7' },
+                { key: 'player_anim_8', duration: 10 }
+            ],
+            frameRate: 16,
+            repeat: -1
+        });
+    
+        self.ship = self.physics.add.sprite(playerInfo.x, playerInfo.y, 'player_anim_1').setOrigin(0.5, 0.5).setDisplaySize(80, 60).play('launch');
         self.ship.body.collideWorldBounds = true;
         self.ship.setBounce(5);
 
         // Add the text to the sprite as a child, just like a group spriteText.x = spriteText.width * -0.5; 
         // Center the text sprite text.y = -10 
         // Position the text 10 pixels above the origin of the sprite
-        if (playerInfo.team === 'blue') {
-            // Set ship colour (Tint)
-            self.ship.setTint(0x0000AA);
-        } else {
-            self.ship.setTint(0xAA0000);
-        }
 
         // We used setDrag, setAngularDrag, and setMaxVelocity to modify how the game object reacts to the arcade physics. 
         self.ship.setDrag(100);
@@ -27,12 +37,7 @@ export default class NetworkManager {
     }
 
     addOtherPlayer(self, playerInfo) {
-        const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(100, 80);
-        if (playerInfo.team === 'blue') {
-            otherPlayer.setTint(0x0000AA);
-        } else {
-            otherPlayer.setTint(0xAA0000);
-        }
+        const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(80, 60);
         otherPlayer.playerId = playerInfo.playerId;
         self.otherPlayers.add(otherPlayer);
     }
@@ -56,12 +61,11 @@ export default class NetworkManager {
     
         // Check up key is down
         if (self.cursors.up.isDown) {
-            self.physics.velocityFromRotation(self.ship.rotation + 1.5, 100, self.ship.body.acceleration);
+            self.physics.velocityFromRotation(self.ship.rotation + 270, 150, self.ship.body.acceleration);
         } else {
             self.ship.setAcceleration(0);
+            // Todo: Turn off engine
         }
-
-        //if (self.cursors.)
     }
 
     publishPlayerMovement(self) {
--- client/javascript/startScene.js
+++ client/javascript/startScene.js
@@ -8,12 +8,33 @@ export default class StartScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('ship', 'assets/player.png');
+        // this.load.image('ship', 'assets/player.png');
+        // this.load.image('sky', 'assets/sky.png');
         this.load.image('otherPlayer', 'assets/player.png');
-        this.load.image('sky', 'assets/sky.png');
         
-        // Finish tomorrow
-        this.load.image('link', 'assets/blue-link.png');
+        this.load.path = 'assets/background/';
+
+        // background animation sprites
+        this.load.image('background_anim_1', 'background_0.png');
+        this.load.image('background_anim_2', 'background_1.png');
+        this.load.image('background_anim_3', 'background_2.png');
+        this.load.image('background_anim_4', 'background_3.png');
+        this.load.image('background_anim_5', 'background_4.png');
+        this.load.image('background_anim_6', 'background_5.png');
+        this.load.image('background_anim_7', 'background_6.png');
+        this.load.image('background_anim_8', 'background_7.png');
+
+        this.load.path = 'assets/player/';
+
+        // Player animation sprites
+        this.load.image('player_anim_1', 'player_0.png');
+        this.load.image('player_anim_2', 'player_1.png');
+        this.load.image('player_anim_3', 'player_2.png');
+        this.load.image('player_anim_4', 'player_3.png');
+        this.load.image('player_anim_5', 'player_4.png');
+        this.load.image('player_anim_6', 'player_5.png');
+        this.load.image('player_anim_7', 'player_6.png');
+        this.load.image('player_anim_8', 'player_7.png');
     }
 
     create() {
@@ -26,8 +47,24 @@ export default class StartScene extends Phaser.Scene {
         // Phaser group - Great for performing multiple operations at the same time.
         this.otherPlayers = this.physics.add.group();
 
-        // Sky
-        this.add.image(0, 0, 'sky').setOrigin(0, 0);
+        // Background Image
+        self.anims.create({
+            key: 'load',
+            frames: [
+                { key: 'background_anim_1' },
+                { key: 'background_anim_2' },
+                { key: 'background_anim_3' },
+                { key: 'background_anim_4' },
+                { key: 'background_anim_5' },
+                { key: 'background_anim_6' },
+                { key: 'background_anim_7' },
+                { key: 'background_anim_8', duration: 10 }
+            ],
+            frameRate: 8,
+            repeat: -1
+        });
+    
+        self.sky = self.physics.add.sprite(0, 0, 'background_anim_1').setOrigin(0, 0).setScale(2, 2).play('load');
 
         // Update current players with new player details.
         this.socket.on('currentPlayers', function(players) {
