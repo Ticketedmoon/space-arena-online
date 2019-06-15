cb6f755362b20b2b0faf26f4fd7926c7b499f366
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-06-15 15:11:26 +0100

    Login screen complete for now

--- client/javascript/loginScene.js
+++ client/javascript/loginScene.js
@@ -15,24 +15,13 @@ export default class LoginScene extends Phaser.Scene {
         let background = this.add.sprite(0, 0, 'background-image');
         background.setOrigin(0, 0);
 
-        let centerX = this.game.canvas.width/2 - 105;
-        let centerY = this.game.canvas.height/2 - 200;
-        this.add.text(centerX, centerY, 'Project NX\n\n',  {
-            font: "5em calibri",
-            align: "center",
-            fill: "lime",
-            stroke: "black",
-            strokeThickness: 2.5
-        });
-
         // Focus
         $("#login-name").focus();
-
         $('#login-button').click(function () {
             // Check if input name box is not empty
             let nameValue = $('#login-name').val();
             if (nameValue.length > 0) {
-                
+
                 // Remove Login UI
                 $('.login-screen-interface').remove();
 
@@ -40,7 +29,8 @@ export default class LoginScene extends Phaser.Scene {
                 self.scene.start('game', nameValue);
             }
             else {
-                // Display warning text specifying that a name value has not been submitted. 
+                // Display warning text specifying that a name value has not been submitted.
+                alert("Please enter a username before joining an online session.");
             }
         });
     }
