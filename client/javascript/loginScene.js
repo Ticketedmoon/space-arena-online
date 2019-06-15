79a9b496f09960d87e8374f95baa82e7285e6f86
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-06-15 16:22:45 +0100

    Allow login with the enter key

--- client/javascript/loginScene.js
+++ client/javascript/loginScene.js
@@ -17,25 +17,37 @@ export default class LoginScene extends Phaser.Scene {
 
         // Focus
         $("#login-name").focus();
+
+        // Clicking the button
         $('#login-button').click(function () {
-            // Check if input name box is not empty
-            let nameValue = $('#login-name').val();
-            if (nameValue.length > 0) {
+            self.attemptLogin(self);
+        });
 
-                // Remove Login UI
-                $('.login-screen-interface').remove();
-
-                // Start game scene
-                self.scene.start('game', nameValue);
-            }
-            else {
-                // Display warning text specifying that a name value has not been submitted.
-                // TODO: Refactor this - Do not use alert() - perhaps display red text on screen, something better.
-                alert("Please enter a username before joining an online session.");
+        // Or pressing the enter key
+        $('#login-name').keypress(function(event) {
+            var keycode = (event.keyCode ? event.keyCode : event.which);
+            if(keycode == '13') {
+                self.attemptLogin(self);
             }
         });
     }
-    
-    update() {
+
+    attemptLogin(self) {
+        // Check if input name box is not empty
+        let nameValue = $('#login-name').val();
+        if (nameValue.length > 0) {
+
+            // Remove Login UI
+            $('.login-screen-interface').remove();
+
+            // Start game scene
+            self.scene.start('game', nameValue);
+        }
+        else {
+            // Display warning text specifying that a name value has not been submitted.
+            // TODO: Refactor this - Do not use alert() - perhaps display red text on screen, something better.
+            alert("Please enter a username before joining an online session.");
+        }
     }
+
 }
